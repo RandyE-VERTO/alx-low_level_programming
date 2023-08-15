0a4fee7ef6557dee68fdf97cd78b88bcb7851282
RandyE-VERTO <randyejejigbe@gmail.com>
2023-08-15 09:51:35 -0700

    Errors encountered

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * Main - starting point
+ * main - starting point
  *
  * Description: this program prints _putchar
  *
@@ -8,15 +8,15 @@
  */
 
 int main(void)
-{	
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-        _putchar('a');
-        _putchar('r');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
