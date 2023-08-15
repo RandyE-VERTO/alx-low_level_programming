3dc5c18ef958a86e6a663f1d855e26ef44f3c3de
RandyE-VERTO <randyejejigbe@gmail.com>
2023-08-15 11:55:22 -0700

    Print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ * Description: The alphabets printed will be in lowercase
+ * Return: 0 (sucessful execution)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
