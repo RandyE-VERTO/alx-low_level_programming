9be4b29a928b20c0b09b587925726f835cdb7710
RandyE-VERTO <randyejejigbe@gmail.com>
2023-08-17 16:40:55 -0700

    Printing numbers is not an easy task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int output = (a * b);
+
+	return (output);
+}
