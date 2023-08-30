03c752c1d842c5ced7e90c4852908c567a2498aa
RandyE-VERTO <randyejejigbe@gmail.com>
2023-08-30 16:41:36 -0700

    Recursion task being recursive

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  _pow_recursion - Returns the value of x
+ *  to power y
+ *
+ *  @x: number input
+ *  @y: power input
+ *
+ *  Return: 0 (success)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
