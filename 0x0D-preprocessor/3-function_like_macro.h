0b0ee68f9101030bb36b97ad0692b8ece1b6a05e
RandyE-VERTO <randyejejigbe@gmail.com>
2023-09-11 16:14:15 -0700

    Pre-processor going great so far

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 #define ABS(num) ((num) > (-1) ? (num) : (num * -1))
-#endif /* This function  computes the absolute value of a number x */
+#endif /* This computes the absolute value of a number */
