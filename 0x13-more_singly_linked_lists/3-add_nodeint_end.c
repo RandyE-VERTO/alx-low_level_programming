7630cdb48c8a0fa10ed8c444db2ac1c8e9809e64
RandyE-VERTO <randyejejigbe@gmail.com>
2023-09-26 02:28:56 -0700

    Errors trailling and betty making sure

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,32 +5,30 @@
  *
  * @head: head of linked list
  * @n: data to be entered in the new node
- *  
+ *
  * Return: address of new element or NULL if it fails
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
-
-	while (temp->next)
+	temp = *head;
+	while (temp->next != NULL)
+	{
 		temp = temp->next;
-
+	}
 	temp->next = new;
-
 	return (new);
 }
