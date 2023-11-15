c7c668cb524a8f56668c20a69828a3e925be0041
RandyE-VERTO <randyeejejigbe@gmail.com>
2023-11-15 09:56:41 -0800

    Recursive journey

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - Write a function that prints a string,
+ *followed by a new line
+ *
+ *@s: string being printed
+ *
+ * Return: 0 always for success
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		*s++;
+		_puts_recursion(s);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
