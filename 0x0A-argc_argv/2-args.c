de0fef08e6075161ce4aad1482ca9f3cfe146b20
RandyE-VERTO <randyeejejigbe@gmail.com>
2023-11-17 13:32:16 -0800

    argc and argv journey

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- prints all arguments it recieves
+ *
+ * @argc: argument count
+ * @argv: Argument array
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
+
