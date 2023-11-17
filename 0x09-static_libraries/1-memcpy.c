a310e40eebed92169e9a85ee3544f07d9b3665fa
RandyE-VERTO <randyeejejigbe@gmail.com>
2023-11-17 09:30:16 -0800

    Working with static libraries

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area
+ *
+ * @dest: memory where copied memory is stored
+ * @src: memory to be copied
+ * @n: no of bytes
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		dest[i] = src[i];
+		n--;
+	}
+
+	return (dest);
+}
