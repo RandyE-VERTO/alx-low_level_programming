c5a08b9d1386a32de556d3c186fa10c4e39f2c29
RandyE-VERTO <randyeejejigbe@gmail.com>
2023-11-17 14:58:59 -0800

    argc and argv journey

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,9 +6,10 @@
  * main- prints the minimum number of coins to make change
  * for an amount of money
  *
+ *@argc: argument count
+ *@argv: argument array
  *
- *
- *
+ * Return: 0 for success
  */
 
 int main(int argc, char *argv[])
