32f09d2c0e80a3e63b0487a07e9be0389f2b01f7
RandyE-VERTO <randyeejejigbe@gmail.com>
2023-11-27 05:54:18 -0800

    Preprocessor for absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,14 @@
 #ifndef MACROE_H
 #define MACROE_H
 
+/**
+ * FILE: functions like a macro
+ *
+ * x: integer input
+ *
+ * Description: Header file computes absolute value of input
+ */
+
 #define ABS(x) (x) < 0 ? (-1) * (x) : (x)
 
 #endif /* MACROE_H */
