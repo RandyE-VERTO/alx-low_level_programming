c96db4fd150476613cdc94c4bbd3b0e85fa8646d
RandyE-VERTO <randyeejejigbe@gmail.com>
2023-11-27 14:42:08 -0800

    Using struct to define and keep info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- creates information about dogs
+ *
+ * @name: Dog name
+ * @age: Dog's age
+ * @owner: name of the owner of the dog
+ *
+ * Description: creating structs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
