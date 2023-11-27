342a4e74c14cee312497f040ef81b95e671d9b4a
RandyE-VERTO <randyeejejigbe@gmail.com>
2023-11-27 06:06:10 -0800

    Preprocessor summation

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,14 +1,6 @@
 #ifndef MACROE_H
 #define MACROE_H
 
-/**
- * FILE: functions like a macro
- *
- * x: integer input
- *
- * Description: Header file computes absolute value of input
- */
-
 #define ABS(x) ((x) < 0 ? (-1) * (x) : (x))
 
 #endif /* MACROE_H */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,7 @@
+#ifndef ADDITIN_H
+#define ADDITION_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* ADDITION_H */
+
